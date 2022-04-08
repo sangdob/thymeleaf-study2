e71fa12cdf98b3a8545d1cce07269884e6fe16c0
sangdob <51118110+sangdob@users.noreply.github.com>
2022-04-09 00:29:36 +0900

    spring init start--

---
+++ thymeleafSpringForm/src/main/java/remote/thymeleafspringform/ThymeleafSpringFormApplication.java
@@ -0,0 +1,13 @@
+package remote.thymeleafspringform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSpringFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafSpringFormApplication.class, args);
+	}
+
+}
---
+++ thymeleafSpringForm/src/test/java/remote/thymeleafspringform/ThymeleafSpringFormApplicationTests.java
@@ -0,0 +1,13 @@
+package remote.thymeleafspringform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSpringFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
