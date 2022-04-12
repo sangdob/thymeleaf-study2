66d478904456867b77575852fd3d7cff274072c8
sangdob <51118110+sangdob@users.noreply.github.com>
2022-04-13 01:16:53 +0900

    타임리프 - 메시지, 국제화 files init

---
+++ message-start/src/main/java/hellomessage/messagestart/MessageStartApplication.java
@@ -0,0 +1,13 @@
+package hellomessage.messagestart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageStartApplication.class, args);
+    }
+
+}
