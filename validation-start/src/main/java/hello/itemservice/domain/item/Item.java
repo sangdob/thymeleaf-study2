a30aae5ae99ab1fc5c63c6c01a03686ee24b5278
sangdob <51118110+sangdob@users.noreply.github.com>
2022-04-20 23:47:14 +0900

    Validation V3
    object error anntation test
    @ScriptAssert()
    object형태의 에러를 match하는 annotation

--- validation-start/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation-start/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,6 +2,7 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
@@ -12,17 +13,25 @@ import javax.validation.constraints.NotNull;
 *   @validated에서 anntation의 등록에 따라 자동으로 동작한다
 * */
 @Data
+@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000이상 이어야 합니다.")
 public class Item {
 
     private Long id;
-
-    @NotBlank(message = "공백 X`")
+    /*
+    * @NotBlank
+    * NotBlank.item.itemName
+    * NotBlank.itemName
+    * NotBlank
+    *
+    * */
+    @NotBlank
     private String itemName;
 
     @NotNull
     @Range(min = 1000, max = 1000000)
     private Long price;
 
+    @NotNull
     @Max(9999)
     private Integer quantity;
 
