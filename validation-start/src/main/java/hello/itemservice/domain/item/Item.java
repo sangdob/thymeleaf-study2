f0501a6920aa85bfaab3647c4d39c412cd1b83ad
sangdob <51118110+sangdob@users.noreply.github.com>
2022-04-20 23:20:41 +0900

    Validation V3 add
    Bean validation start
    
    hibernate anntation활용

--- validation-start/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation-start/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,29 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+
+/*
+*   @validated에서 anntation의 등록에 따라 자동으로 동작한다
+* */
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank(message = "공백 X`")
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Long price;
+
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
---
+++ validation-start/src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,33 @@
+package hello.itemservice.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public class BeanValidationTest {
+
+    /*
+    * Bean Validation 기본기능 테스트
+    * */
+    @Test
+    void beanValidation() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Item item = new Item();
+        item.setItemName(" ");
+        item.setPrice(0L);
+        item.setQuantity(10000);
+
+        Set<ConstraintViolation<Item>> violations = validator.validate(item);
+        for (var violation : violations) {
+            System.out.println("violation = " + violation);
+            System.out.println("violation = " + violation.getMessage());
+        }
+    }
+}
