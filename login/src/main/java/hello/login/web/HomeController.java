9f98e129062484c51fc5515fcaab79492f309ad7
sangdob <51118110+sangdob@users.noreply.github.com>
2022-05-03 00:12:40 +0900

    ArgumentResolver적용 interceptor의 resolver를 활용할 경우 사용하게 된다.

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
@@ -71,8 +72,17 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+   // @GetMapping("/")
     public String homeLoginV3(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member, Model model) {
+         if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+        return "loginHome";
+    }
+    @GetMapping("/")
+    public String homeLoginV3ArgumentResolver(@Login Member member, Model model) {
          if (member == null) {
             return "home";
         }
