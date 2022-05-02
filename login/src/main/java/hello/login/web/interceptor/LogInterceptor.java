ca87748f46853d36ac8475b8e61e30ab67d2308d
sangdob <51118110+sangdob@users.noreply.github.com>
2022-05-02 23:29:49 +0900

    스프링의 interceptor기능을 활용하여 컨트롤러가 실행이전에 필요한 항목을 처리 할 수 있다.
    web -> was -> filter -> servlet -> interceptor -> controller

---
+++ login/src/main/java/hello/login/web/interceptor/LogInterceptor.java
@@ -0,0 +1,49 @@
+package hello.login.web.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+    public static final String LOG_ID = "logId";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        request.setAttribute(LOG_ID, uuid);
+
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+        }
+
+        log.info("request [{}] [{}] [{}]", uuid, requestURI, handler);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("post handler [{}]", modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        String logId = request.getAttribute(LOG_ID).toString();
+
+        log.info("Response [{}] [{}] [{}]", logId, requestURI, handler);
+
+        if (ex != null) {
+            log.error("afterCompletion Error !!",ex);
+        }
+    }
+}
