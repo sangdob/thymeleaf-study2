738113342b39984c0b7991e410f6047a84492e37
snagdob <skql1358@gmail.com>
2022-05-05 10:53:01 +0900

    exception 404, 500에러 띄우기

--- exception/src/main/java/hello/exception/servlet/ServletExController.java
+++ exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Slf4j
 @Controller
 public class ServletExController {
@@ -11,6 +14,18 @@ public class ServletExController {
     @GetMapping("/error-ex")
     public void errorEx() {
         throw new RuntimeException("exception  !!");
+    }
+
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse httpServletResponse) throws IOException {
+
+        httpServletResponse.sendError(404, "404 error!!");
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse httpServletResponse) throws IOException {
+
+        httpServletResponse.sendError(500);
     }
 
 }
