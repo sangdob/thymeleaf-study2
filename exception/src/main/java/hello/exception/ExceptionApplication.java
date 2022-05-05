d45dd03c414063a884853164bdcdeb9f09849a39
snagdob <skql1358@gmail.com>
2022-05-05 10:36:58 +0900

    예외 예제 프로젝트 그룹네임 변경

--- exception/src/main/java/hello/exception/ExceptionApplication.java
+++ exception/src/main/java/hello/exception/ExceptionApplication.java
@@ -1,4 +1,4 @@
-package hellloo.exception;
+package hello.exception;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- exception/src/test/java/hello/exception/ExceptionApplicationTests.java
+++ exception/src/test/java/hello/exception/ExceptionApplicationTests.java
@@ -1,4 +1,4 @@
-package hellloo.exception;
+package hello.exception;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
