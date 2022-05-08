38dc158ee4d6e8e898180ab2d398360174d7ba2b
sangdob <51118110+sangdob@users.noreply.github.com>
2022-05-08 23:44:15 +0900

    에러페이지 추가

--- login/src/main/java/hello/login/web/WebConfig.java
+++ login/src/main/java/hello/login/web/WebConfig.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver());
