f3a037d58ff386de26f39e351eab94238369288f
sangdob <51118110+sangdob@users.noreply.github.com>
2022-05-11 00:09:53 +0900

    error interceptor처리 방식

---
+++ exception/src/main/java/hello/exception/interceptor/LogInterceptor.java
@@ -0,0 +1,26 @@
+package hello.exception.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+    public static final String LOG_ID = "LogId";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String requestURI = request.getRequestURI();
+
+        String uuid = UUID.randomUUID().toString();
+        request.setAttribute(LOG_ID, uuid);
+
+        log.info("request [{}] [{}] [{}] [{}]", uuid, request.getDispatcherType(), requestURI, handler);
+        return true;
+    }
+}
