501b907465a531555ddc00442ae50da4de5f87c9
sangdob <51118110+sangdob@users.noreply.github.com>
2022-05-28 00:00:06 +0900

    멀티 파일에 관한 처리

--- upload/src/main/java/hello/upload/controller/ItemController.java
+++ upload/src/main/java/hello/upload/controller/ItemController.java
@@ -1,14 +1,22 @@
 package hello.upload.controller;
 
+import hello.upload.domain.Item;
 import hello.upload.domain.ItemRepository;
+import hello.upload.domain.UploadFile;
 import hello.upload.file.FileStore;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -24,7 +32,35 @@ public class ItemController {
     }
 
     @PostMapping("/items/new")
-    public String saveItem(@ModelAttribute ItemForm form, RedirectAttributes redirectAttributes) {
-        return "";
+    public String saveItem(@ModelAttribute ItemForm form, RedirectAttributes redirectAttributes) throws IOException {
+        UploadFile attachFile = fileStore.storeFile(form.getAttachFile());
+        List<UploadFile> storeFiles = fileStore.storeFiles(form.getImageFiles());
+
+        Item item = new Item();
+        item.setItemName(form.getItemName());
+        item.setAttachFile(attachFile);
+        item.setImageFiles(storeFiles);
+        itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId", item.getId());
+
+        log.info("Itemform = {}", form);
+
+        return "redirect:/items/{itemId}";
     }
+
+    @GetMapping("/items/{id}")
+    public String items(@PathVariable Long id, Model model) {
+        Item item = itemRepository.findById(id);
+        model.addAttribute("item", item);
+
+        return "item-view";
+    }
+
+    @ResponseBody
+    @GetMapping("/images/{fileName}")
+    public Resource downloadImage(@PathVariable String fileName) throws MalformedURLException {
+        return new UrlResource("file" + fileStore.getFullPath(fileName));
+    }
+
 }
--- upload/src/main/java/hello/upload/domain/Item.java
+++ upload/src/main/java/hello/upload/domain/Item.java
@@ -2,11 +2,13 @@ package hello.upload.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Item {
     private Long id;
     private String itemName;
